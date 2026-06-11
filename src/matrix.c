e535ba34ed1b72b5571ea9d9150091522b135281
MayconXis <md7111016@gmail.com>
2026-06-11 10:36:46 -0300

    Refatorando Matrix.h

---
+++ src/matrix.c
@@ -0,0 +1,28 @@
+
+// parar guardar os dados da matriz
+typedef struct {
+    double dados[100][100];
+    int linhas;
+    int colunas;
+}Matriz;
+
+//termo independentes
+typedef struct {
+    double vetor[100];
+    int tam;
+}Vetor;
+
+// struct utilizando os dados da matriz e os termos independentes juntos (sistema Ax = b)
+typedef struct {
+    Matriz A;
+    Vetor b;
+    int num_variaveis;
+    int num_equacoes;
+}SistemaLinear;
+
+
+
+
+
+
+
